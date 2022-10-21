4f51114c6bed15cd41d624126309fe071db381f8
Gregory Mallios <gregmallios@gmail.com>
2022-10-22 02:02:32 +0300

    Add clamping function

--- src/utils.rs
+++ src/utils.rs
@@ -18,3 +18,23 @@ pub(crate) fn mac_str_to_u64(addr: &str) -> Result<u64, std::num::ParseIntError>
     let a = u64::from_str_radix(&addr.replace(":", ""), 16)?;
     Ok(a)
 }
+
+
+pub(crate) trait Clamp<T> {
+    fn clamp(self, min: T, max: T) -> T;
+}
+
+impl<T> Clamp<T> for T
+where
+    T: PartialOrd + Copy,
+{
+    fn clamp(self, min: T, max: T) -> T {
+        if self > max {
+            max
+        } else if self < min {
+            min
+        } else {
+            self
+        }
+    }
+}
