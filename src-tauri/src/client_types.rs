3e9b64ba0e67c209afbc612739ea80f2a668620e
Gregory Mallios <gregmallios@gmail.com>
2022-10-24 19:49:08 +0300

    Tauri backend work

---
+++ src-tauri/src/client_types.rs
@@ -0,0 +1,26 @@
+use soundcore_lib::A3951::{A3951BatteryLevel, A3951BatteryCharging};
+use ts_rs::TS;
+use serde::{Serialize, Deserialize};
+// Run cargo test inside src-tauri to generate the typescript definitions
+#[derive(TS, Serialize, Deserialize)]
+#[ts(export, export_to = "../src/bindings/DeviceSelection.d.ts")]
+pub(crate) enum DeviceSelection {
+    A3951,
+    None
+}
+
+// #[derive(TS, Serialize, Deserialize)]
+// #[ts(export, export_to = "../src/bindings/Result.d.ts")]
+// pub(crate) enum Result {
+//     Ok,
+//     Error
+// }
+
+// #[derive(TS, Serialize, Deserialize)]
+// #[ts(export, export_to = "../src/bindings/BatteryInfo.d.ts")]
+// pub(crate) struct BatteryLevelResponse {
+//     pub(crate) level: A3951BatteryLevel,
+//     pub(crate) charging: A3951BatteryCharging
+// }
+
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,124 @@
     windows_subsystem = "windows"
 )]
 
-#[cfg(target_os = "windows")]
-use soundcore_lib::A3951;
+use std::sync::{Mutex, RwLock};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+
+use client_types::{DeviceSelection};
+use soundcore_lib::A3951::A3951BatteryLevel;
+#[cfg(target_os = "windows")]
+use soundcore_lib::A3951::A3951Device;
+use tauri::State;
+
+mod client_types;
+
+
+
+enum SupportedDevices {
+    A3951(A3951Device),
 }
+
+
+
+#[tauri::command]
+fn init_device(state: State<DeviceState>, device: DeviceSelection) -> Result<String, String> {
+    let mut device_state = state.device.lock().unwrap();
+    match device {
+        DeviceSelection::A3951 => {
+            let device = A3951Device::new();
+            match device {
+                Ok(device) => {
+                    *device_state = Some(Mutex::new(SupportedDevices::A3951(device)));
+                    *state.initialized.lock().unwrap() = true;
+                    println!("Device initialized");
+                    Ok("A3951 Device initialized".to_string())
+                }
+                Err(_) => {
+                    *device_state = None;
+                    *state.initialized.lock().unwrap() = false;
+                    Err("Failed to initialize device".to_string())
+                },
+            }
+        },
+        DeviceSelection::None => {
+            *device_state = None;
+            *state.initialized.lock().unwrap() = false;
+            Err("Nothing to initialize".to_string())
+        }
+    }
+}
+
+#[tauri::command]
+fn connect_uuid(state: State<DeviceState>, mac_addr: String, uuid: String) -> Result<(), String>{
+    let mut state = state.device.lock().unwrap();
+    let device_state = state.as_ref();
+    match device_state{
+        Some(selected_device) => {
+            let mut guard = selected_device.lock().unwrap();
+            let mut selected_device = &mut *guard;
+            match selected_device {
+                SupportedDevices::A3951(selected_device) => {
+                    match selected_device.connect_uuid(&mac_addr, &uuid) {
+                        Ok(_) => {
+                            println!("Connected to device");
+                            Ok(())
+                        },
+                        Err(_) => {
+                            println!("Failed to connect to device");
+                            Err("Failed to connect to device".to_string())
+                        }
+                    }
+                }
+            }
+        },
+        None => {
+            println!("Device not initialized");
+            Err("Device not initialized".to_string())
+        }
+    }
+}
+
+
+// Generalizing this requires generalizing soundcore-lib types
+#[tauri::command]
+fn get_battery_level(state: State<DeviceState>) -> Result<A3951BatteryLevel, String> {
+    let device_state = state.device.lock().unwrap();
+    match &*device_state {
+        Some(device) => {
+            let device = device.lock().unwrap();
+            match &*device {
+                SupportedDevices::A3951(device) => {
+                    println!("Getting battery level");
+                    let battery_level = device.get_battery_level();
+                    match battery_level {
+                        Ok(battery_level) => {
+                            Ok(battery_level)
+                        },
+                        Err(_) => {
+                            println!("Failed to get battery level");
+                            Err("Failed to get battery info".to_string())
+                        }
+                    }
+                }
+            }
+        },
+        None => {
+            Err("Device not initialized".to_string())
+        }
+    }
+}
+
+struct DeviceState {
+    device: Mutex<Option<Mutex<SupportedDevices>>>,
+    initialized: Mutex<bool>,
+}
+
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(DeviceState { device: Mutex::new(None), initialized: Mutex::new(false) })
+        .invoke_handler(tauri::generate_handler![init_device, connect_uuid, get_battery_level])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
