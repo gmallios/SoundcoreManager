91aceb57e69d52e786aaa4eb1ae6bde6b9404d6b
Gregory Mallios <gregmallios@gmail.com>
2022-10-26 18:27:37 +0300

    Cleanup A3951InfoCard component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import A3951InfoCard from "./components/A3951InfoCard";
 import useDeviceStore, { DeviceConnectionState } from "./hooks/useDeviceStore";
-
+import Stack from '@mui/material/Stack';
+import ANCModeCard from "./components/ANCModeCard";
 
 function App() {
   const { tryInitialize, getBatteryLevel, getBatteryCharging, connectUUID, deviceConnectionState } = useDeviceStore();
@@ -13,15 +14,11 @@ function App() {
   useEffect(() => {
     tryInitialize("A3951");
     connectUUID("AC:12:2F:6A:D2:07","00001101-0000-1000-8000-00805F9B34FB");
-
-    
   }, []);
 
   // May require additional tweaking
   const BATTERY_LEVEL_POLL_RATE = 10000;
   const BATTERY_CHARGING_POLL_RATE = 500;
-
-
 
   useEffect(() => {
     if(deviceConnectionState == DeviceConnectionState.CONNECTED){
@@ -45,7 +42,10 @@ function App() {
 
   return (
     <div>
-      <A3951InfoCard />
+      <Stack>
+        <A3951InfoCard />
+        <ANCModeCard />
+      </Stack>
     </div>
   );
 }
--- src/components/A3951InfoCard.tsx
+++ src/components/A3951InfoCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Paper, Typography } from "@mui/material";
 import useDeviceStore from "../hooks/useDeviceStore";
 import A3951ImgLeft from "./../assets/a3951_img_device_left.webp";
 import A3951ImgRight from "./../assets/a3951_img_device_right_edited.webp";
@@ -20,32 +20,39 @@ import BatteryUnknownIcon from '@mui/icons-material/BatteryUnknown';
 export default function A3951InfoCard() {
     const { batteryLevel, batteryCharging } = useDeviceStore();
     return (
-        <Card sx={{ minWidth: 275, margin: 2 }}>
-            <CardContent>
-                <Grid container spacing={2} justifyContent="center" alignItems="center">
-                    <GridItem img={A3951ImgLeft} infoAlign="left" batteryLevel={batteryLevel.left} batteryCharging={batteryCharging.left} />
-                    <GridItem img={A3951ImgRight} infoAlign="right" batteryLevel={batteryLevel.right}  batteryCharging={batteryCharging.right}/>
-                </Grid>
-            </CardContent>
-            {/* <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions> */}
-        </Card>
+        // As card
+        // <Card sx={{ minWidth: 275, margin: 1.5 }}>
+        //     <CardContent>
+        //         <Grid container spacing={2} justifyContent="center" alignItems="center">
+        //             <EarbudItem img={A3951ImgLeft} alignTo="left" batteryLevel={batteryLevel.left} batteryCharging={batteryCharging.left} />
+        //             <EarbudItem img={A3951ImgRight} alignTo="right" batteryLevel={batteryLevel.right}  batteryCharging={batteryCharging.right}/>
+        //         </Grid>
+        //     </CardContent>
+        //     {/* <CardActions>
+        //   <Button size="small">Learn More</Button>
+        // </CardActions> */}
+        // </Card>
+        <Box>
+            <Paper sx={{ display: "flex", margin: 1.5, justifyContent: "center", alignItems: "center" }} elevation={0}>
+                <EarbudItem img={A3951ImgLeft} alignTo="left" batteryLevel={batteryLevel.left} batteryCharging={batteryCharging.left} />
+                <EarbudItem img={A3951ImgRight} alignTo="right" batteryLevel={batteryLevel.right} batteryCharging={batteryCharging.right} />
+            </Paper>
+        </Box>
     )
 }
 
-function GridItem(props: any) {
+function EarbudItem({ alignTo, batteryLevel, batteryCharging, img }: { alignTo: "left" | "right", batteryLevel: number, batteryCharging: boolean, img: string }) {
     return (
         <Grid item>
             <Grid container spacing={1} justifyContent="center" alignItems="center">
-                {props.infoAlign == "left" &&
-                    <EarpiceInfo batteryLevel={props.batteryLevel} batteryCharging={props.batteryCharging}/>
+                {alignTo == "left" &&
+                    <EarpiceInfo batteryLevel={batteryLevel} batteryCharging={batteryCharging} />
                 }
                 <Grid item>
-                    <img src={props.img} alt="A3951" height={80} />
+                    <img src={img} alt="A3951" height={80} />
                 </Grid>
-                {props.infoAlign == "right" &&
-                    <EarpiceInfo batteryLevel={props.batteryLevel} batteryCharging={props.batteryCharging} />
+                {alignTo == "right" &&
+                    <EarpiceInfo batteryLevel={batteryLevel} batteryCharging={batteryCharging} />
                 }
             </Grid>
         </Grid>
@@ -57,22 +64,22 @@ function EarpiceInfo({ batteryLevel, batteryCharging }: any) {
     //0-5
     switch (batteryLevel) {
         case 0:
-            icon =  !batteryCharging? <BatteryAlertIcon  /> : <BatteryCharging20Icon  />;
+            icon = !batteryCharging ? <BatteryAlertIcon /> : <BatteryCharging20Icon />;
             break;
         case 1:
-            icon =  !batteryCharging? <Battery20Icon  /> : <BatteryCharging20Icon  />;
+            icon = !batteryCharging ? <Battery20Icon /> : <BatteryCharging20Icon />;
             break;
         case 2:
-            icon =  !batteryCharging? <Battery30Icon /> : <BatteryCharging30Icon />;
+            icon = !batteryCharging ? <Battery30Icon /> : <BatteryCharging30Icon />;
             break;
         case 3:
-            icon =  !batteryCharging? <Battery60Icon  /> : <BatteryCharging60Icon  />;
+            icon = !batteryCharging ? <Battery60Icon /> : <BatteryCharging60Icon />;
             break;
         case 4:
-            icon = !batteryCharging? <Battery80Icon /> : <BatteryCharging80Icon  />;
+            icon = !batteryCharging ? <Battery80Icon /> : <BatteryCharging80Icon />;
             break;
         case 5:
-            icon = !batteryCharging? <BatteryFullIcon  /> : <BatteryChargingFullIcon />;
+            icon = !batteryCharging ? <BatteryFullIcon /> : <BatteryChargingFullIcon />;
             break;
         default:
             icon = <BatteryUnknownIcon />;
@@ -83,7 +90,7 @@ function EarpiceInfo({ batteryLevel, batteryCharging }: any) {
         <Grid item>
             {icon}
             <Typography variant="body2" color="text.secondary">
-                {batteryLevel*2*10}%
+                {batteryLevel * 2 * 10}%
             </Typography>
         </Grid>
     )
