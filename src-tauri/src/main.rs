643f4ce6c1f9fed03f1726000b84f3b3a74a1b19
Gregory Mallios <gregmallios@gmail.com>
2022-11-14 01:46:14 +0200

    backend: Refactor to acount for soundcore-lib changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,31 +3,52 @@
     windows_subsystem = "windows"
 )]
 
-use std::sync::{Mutex};
+#[macro_use]
+extern crate lazy_static;
 
+use std::sync::{Arc, Mutex, RwLock};
 
-use client_types::{DeviceSelection, ANCModes};
+use bluetooth_lib::{BluetoothAdrr, RFCOMM};
+use client_types::{ANCModes, DeviceSelection};
+use soundcore_lib::types::{
+    ANCProfile, BatteryCharging, BatteryLevel, DeviceInfo, DeviceStatus, EQWave,
+};
 #[cfg(target_os = "windows")]
 use soundcore_lib::A3951::A3951Device;
-use soundcore_lib::types::{BatteryLevel, BatteryCharging, DeviceStatus, DeviceInfo, ANCProfile, EQWave};
-use tauri::State;
+use tauri::{Manager, State};
 
 mod client_types;
 
-
-
-enum SupportedDevices {
-    A3951(A3951Device),
+enum SupportedDevices<'a> {
+    A3951(A3951Device<'a>),
 }
 
-
+fn send_rfcomm(data: &[u8]) -> Result<(), soundcore_lib::error::SoundcoreError> {
+    let comm = RFCOMM_STATE.lock().unwrap();
+    match comm.send(data) {
+        Ok(_) => Ok(()),
+        Err(_e) => Err(soundcore_lib::error::SoundcoreError::SendError),
+    }
+}
+fn recv_rfcomm(num_of_bytes: usize) -> Result<Vec<u8>, soundcore_lib::error::SoundcoreError> {
+    let comm = RFCOMM_STATE.lock().unwrap();
+    match comm.recv(num_of_bytes) {
+        Ok(data) => Ok(data),
+        Err(_e) => Err(soundcore_lib::error::SoundcoreError::RecvError),
+    }
+}
 
 #[tauri::command]
 fn init_device(state: State<DeviceState>, device: DeviceSelection) -> Result<String, String> {
     let mut device_state = state.device.lock().unwrap();
+    let mut comm = RFCOMM_STATE.lock().unwrap();
+    match comm.create_rfcomm_socket() {
+        Ok(c) => *comm = c,
+        Err(e) => return Err(e.to_string()),
+    }
     match device {
         DeviceSelection::A3951 => {
-            let device = A3951Device::new();
+            let device = A3951Device::new(&send_rfcomm, &recv_rfcomm);
             match device {
                 Ok(device) => {
                     *device_state = Some(Mutex::new(SupportedDevices::A3951(device)));
@@ -38,9 +59,9 @@ fn init_device(state: State<DeviceState>, device: DeviceSelection) -> Result<Str
                     *device_state = None;
                     *state.initialized.lock().unwrap() = false;
                     Err("Failed to initialize device".to_string())
-                },
+                }
             }
-        },
+        }
         DeviceSelection::None => {
             *device_state = None;
             *state.initialized.lock().unwrap() = false;
@@ -50,32 +71,13 @@ fn init_device(state: State<DeviceState>, device: DeviceSelection) -> Result<Str
 }
 
 #[tauri::command]
-fn connect_uuid(state: State<DeviceState>, mac_addr: String, uuid: String) -> Result<(), String>{
-    let state = state.device.lock().unwrap();
-    let device_state = state.as_ref();
-    match device_state{
-        Some(selected_device) => {
-            let mut guard = selected_device.lock().unwrap();
-            let selected_device = &mut *guard;
-            match selected_device {
-                SupportedDevices::A3951(selected_device) => {
-                    match selected_device.connect_uuid(&mac_addr, &uuid) {
-                        Ok(_) => {
-                            Ok(())
-                        },
-                        Err(_) => {
-                            Err("Failed to connect to device".to_string())
-                        }
-                    }
-                }
-            }
-        },
-        None => {
-            Err("Device not initialized".to_string())
-        }
+fn connect_uuid(mac_addr: String, uuid: String) -> Result<(), String> {
+    let mut comm = RFCOMM_STATE.lock().unwrap();
+    match comm.connect_uuid(BluetoothAdrr::from(mac_addr), &uuid) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.to_string()),
     }
 }
-
 
 // Generalizing this requires generalizing soundcore-lib types
 #[tauri::command]
@@ -88,19 +90,13 @@ fn get_battery_level(state: State<DeviceState>) -> Result<BatteryLevel, String> 
                 SupportedDevices::A3951(device) => {
                     let battery_level = device.get_battery_level();
                     match battery_level {
-                        Ok(battery_level) => {
-                            Ok(battery_level)
-                        },
-                        Err(_) => {
-                            Err("Failed to get battery info".to_string())
-                        }
+                        Ok(battery_level) => Ok(battery_level),
+                        Err(_) => Err("Failed to get battery info".to_string()),
                     }
                 }
             }
-        },
-        None => {
-            Err("Device not initialized".to_string())
         }
+        None => Err("Device not initialized".to_string()),
     }
 }
 
@@ -114,19 +110,13 @@ fn get_battery_charging(state: State<DeviceState>) -> Result<BatteryCharging, St
                 SupportedDevices::A3951(device) => {
                     let battery_charging = device.get_battery_charging();
                     match battery_charging {
-                        Ok(battery_charging) => {
-                            Ok(battery_charging)
-                        },
-                        Err(_) => {
-                            Err("Failed to get battery charging".to_string())
-                        }
+                        Ok(battery_charging) => Ok(battery_charging),
+                        Err(_) => Err("Failed to get battery charging".to_string()),
                     }
                 }
             }
-        },
-        None => {
-            Err("Device not initialized".to_string())
         }
+        None => Err("Device not initialized".to_string()),
     }
 }
 
@@ -140,19 +130,13 @@ fn get_device_status(state: State<DeviceState>) -> Result<DeviceStatus, String> 
                 SupportedDevices::A3951(device) => {
                     let device_status = device.get_status();
                     match device_status {
-                        Ok(device_status) => {
-                            Ok(device_status)
-                        },
-                        Err(_) => {
-                            Err("Failed to get device status".to_string())
-                        }
+                        Ok(device_status) => Ok(device_status),
+                        Err(_) => Err("Failed to get device status".to_string()),
                     }
                 }
             }
-        },
-        None => {
-            Err("Device not initialized".to_string())
         }
+        None => Err("Device not initialized".to_string()),
     }
 }
 
@@ -166,19 +150,13 @@ fn get_device_info(state: State<DeviceState>) -> Result<DeviceInfo, String> {
                 SupportedDevices::A3951(device) => {
                     let device_info = device.get_info();
                     match device_info {
-                        Ok(device_info) => {
-                            Ok(device_info)
-                        },
-                        Err(_) => {
-                            Err("Failed to get device info".to_string())
-                        }
+                        Ok(device_info) => Ok(device_info),
+                        Err(_) => Err("Failed to get device info".to_string()),
                     }
                 }
             }
-        },
-        None => {
-            Err("Device not initialized".to_string())
         }
+        None => Err("Device not initialized".to_string()),
     }
 }
 
@@ -198,21 +176,23 @@ fn get_anc_mode(state: State<DeviceState>) -> Result<ANCModes, String> {
                                 ANCProfile::ANC_OUTDOOR_MODE => ANCModes::AncOutdoorMode,
                                 ANCProfile::ANC_TRANSPORT_MODE => ANCModes::AncTransportMode,
                                 ANCProfile::NORMAL_MODE => ANCModes::NormalMode,
-                                ANCProfile::TRANSPARENCY_FULLY_TRANSPARENT_MODE => ANCModes::TransparencyFullyTransparentMode,
-                                ANCProfile::TRANSPARENCY_VOCAL_MODE => ANCModes::TransparencyVocalMode,
-                                custom_val => {
-                                    ANCModes::AncCustomValue(custom_val.anc_custom)
+                                ANCProfile::TRANSPARENCY_FULLY_TRANSPARENT_MODE => {
+                                    ANCModes::TransparencyFullyTransparentMode
                                 }
+                                ANCProfile::TRANSPARENCY_VOCAL_MODE => {
+                                    ANCModes::TransparencyVocalMode
+                                }
+                                custom_val => ANCModes::AncCustomValue(custom_val.anc_custom),
                             };
                             return Ok(anc_mode_selected);
-                        },
+                        }
                         Err(_) => {
                             return Err("Failed to get anc mode".to_string());
                         }
                     }
                 }
             }
-        },
+        }
         None => {
             return Err("Device not initialized".to_string());
         }
@@ -233,66 +213,64 @@ fn set_anc_mode(state: State<DeviceState>, mode: ANCModes) -> Result<(), String>
                         ANCModes::AncOutdoorMode => ANCProfile::ANC_OUTDOOR_MODE,
                         ANCModes::AncIndoorMode => ANCProfile::ANC_INDOOR_MODE,
                         ANCModes::AncCustomValue(value) => ANCProfile::anc_custom_value(value),
-                        ANCModes::TransparencyFullyTransparentMode => ANCProfile::TRANSPARENCY_FULLY_TRANSPARENT_MODE,
+                        ANCModes::TransparencyFullyTransparentMode => {
+                            ANCProfile::TRANSPARENCY_FULLY_TRANSPARENT_MODE
+                        }
                         ANCModes::TransparencyVocalMode => ANCProfile::TRANSPARENCY_VOCAL_MODE,
                     };
                     match device.set_anc(mode_to_send) {
-                        Ok(_) => {
-                            Ok(())
-                        },
-                        Err(_) => {
-                            Err("Failed to set ANC mode".to_string())
-                        }
+                        Ok(_) => Ok(()),
+                        Err(_) => Err("Failed to set ANC mode".to_string()),
                     }
                 }
             }
-        },
-        None => {
-            Err("Device not initialized".to_string())
         }
+        None => Err("Device not initialized".to_string()),
     }
 }
 
 #[tauri::command]
-fn set_eq_wave(state: State<DeviceState>, eq: EQWave) -> Result<(), String>{
+fn set_eq_wave(state: State<DeviceState>, eq: EQWave) -> Result<(), String> {
     let device_state = state.device.lock().unwrap();
     match &*device_state {
         Some(device) => {
             let device = device.lock().unwrap();
             match &*device {
-                SupportedDevices::A3951(device) => {
-                    match device.set_eq(eq) {
-                        Ok(_) => {
-                            Ok(())
-                        },
-                        Err(_) => {
-                            Err("Failed to set EQ wave".to_string())
-                        }
-                    }
-                }
+                SupportedDevices::A3951(device) => match device.set_eq(eq) {
+                    Ok(_) => Ok(()),
+                    Err(_) => Err("Failed to set EQ wave".to_string()),
+                },
             }
-        },
-        None => {
-            Err("Device not initialized".to_string())
         }
+        None => Err("Device not initialized".to_string()),
     }
 }
 
-struct DeviceState {
-    device: Mutex<Option<Mutex<SupportedDevices>>>,
+struct DeviceState<'a> {
+    device: Arc<Mutex<Option<Mutex<SupportedDevices<'a>>>>>,
     initialized: Mutex<bool>,
 }
 
-
+lazy_static! {
+    static ref RFCOMM_STATE: Arc<Mutex<RFCOMM>> = Arc::new(Mutex::new(RFCOMM::new()));
+}
 
 fn main() {
     tauri::Builder::default()
-        .manage(DeviceState { device: Mutex::new(None), initialized: Mutex::new(false) })
+        .manage(DeviceState {
+            device: Arc::new(Mutex::new(None)),
+            initialized: Mutex::new(false),
+        })
         .invoke_handler(tauri::generate_handler![
-            init_device, connect_uuid,
-            get_device_status, get_device_info,
-            get_battery_level, get_battery_charging, 
-            set_anc_mode, get_anc_mode, set_eq_wave
+            init_device,
+            connect_uuid,
+            get_device_status,
+            get_device_info,
+            get_battery_level,
+            get_battery_charging,
+            set_anc_mode,
+            get_anc_mode,
+            set_eq_wave,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
