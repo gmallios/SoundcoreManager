58146ccca3ab3879476037b61c52a228c0acec30
Gregory Mallios <gregmallios@gmail.com>
2022-11-14 01:44:25 +0200

    soundcore-lib: Add basic example

---
+++ soundcore-lib/examples/basic.rs
@@ -0,0 +1,39 @@
+use bluetooth_lib::{RFCOMM};
+use soundcore_lib::{error::SoundcoreError, A3951::A3951Device};
+
+const UUID: &str = "00001101-0000-1000-8000-00805F9B34FB";
+const BT_ADDR: &str = "AC:12:2F:6A:D2:07";
+
+fn main() {
+    println!("Fetching device info...");
+
+    // Initialize the RFCOMM communcation channel
+    let mut comm = RFCOMM::new().create_rfcomm_socket().unwrap();
+
+    let addr = bluetooth_lib::BluetoothAdrr::from(BT_ADDR);
+    // Connect to device UUID service
+    comm.connect_uuid(addr, UUID).unwrap();
+
+    // Setup send and receive functions for soundcore-lib, we use our own RFCOMM implementation
+    // Feel free to use any other implementation
+    let mut send_fn = |data: &[u8]| match comm.send(data) {
+        Ok(_) => Ok(()),
+        Err(_e) => Err(SoundcoreError::SendError),
+    };
+    let mut recv_fn = |num_of_bytes: usize| match comm.recv(num_of_bytes) {
+        Ok(data) => Ok(data),
+        Err(_e) => Err(SoundcoreError::RecvError),
+    };
+
+    let mut device = A3951Device::new(&mut send_fn, &mut recv_fn).unwrap();
+
+    match device.get_info() {
+        Ok(info) => println!(
+            "SN: {}, Right FW: {}, Left FW: {}",
+            info.sn, info.right_fw, info.left_fw
+        ),
+        Err(e) => println!("Error: {:?}", e),
+    }
+
+    comm.close();
+}
