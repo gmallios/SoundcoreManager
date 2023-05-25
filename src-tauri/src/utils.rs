24d385fef50027deb5dc160130d805d33b9c4275
Grigorios Mallios <gregmallios@gmail.com>
2023-05-25 20:46:52 +0300

    fix: Do proper matching in anc_profile_to_mode

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,6 +1,8 @@
 use crate::frontend_types::ANCModes;
 use soundcore_lib::types::ANCProfile;
 
+// TODO: ANCModes should carry the saved CustomValue as well in order to send it
+// TODO: Cleanup ANC types
 pub(crate) fn anc_mode_to_profile(mode: ANCModes) -> ANCProfile {
     match mode {
         ANCModes::NormalMode => ANCProfile::NORMAL_MODE,
@@ -15,16 +17,16 @@ pub(crate) fn anc_mode_to_profile(mode: ANCModes) -> ANCProfile {
     }
 }
 
+
 pub(crate) fn anc_profile_to_mode(profile: ANCProfile) -> ANCModes {
     match profile {
-        ANCProfile::ANC_INDOOR_MODE => ANCModes::AncIndoorMode,
-        ANCProfile::ANC_OUTDOOR_MODE => ANCModes::AncOutdoorMode,
-        ANCProfile::ANC_TRANSPORT_MODE => ANCModes::AncTransportMode,
-        ANCProfile::NORMAL_MODE => ANCModes::NormalMode,
-        ANCProfile::TRANSPARENCY_FULLY_TRANSPARENT_MODE => {
-            ANCModes::TransparencyFullyTransparentMode
-        }
-        ANCProfile::TRANSPARENCY_VOCAL_MODE => ANCModes::TransparencyVocalMode,
-        custom_val => ANCModes::AncCustomValue(custom_val.anc_custom),
+        ANCProfile{ option: 0, anc_option: 0, .. } => ANCModes::AncTransportMode,
+        ANCProfile{ option: 0, anc_option: 1, .. } => ANCModes::AncOutdoorMode,
+        ANCProfile{ option: 0, anc_option: 2, .. } => ANCModes::AncIndoorMode,
+        ANCProfile{ option: 0, anc_option: 3, .. } => ANCModes::AncCustomValue(profile.anc_custom),
+        ANCProfile{ option: 1, transparency_option: 0, .. } => ANCModes::TransparencyFullyTransparentMode,
+        ANCProfile{ option: 1, transparency_option: 1, .. } => ANCModes::TransparencyVocalMode,
+        ANCProfile{ option: 2, .. } => ANCModes::NormalMode,
+        _ => unreachable!(),
     }
 }
