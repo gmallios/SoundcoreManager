cf30f30ba66566e647a8bd32f676c78628084b64
Grigorios Mallios <gregmallios@gmail.com>
2023-10-28 13:33:16 +0300

    fix(parser): :bug: Fix checksum parser

--- soundcore-lib/src/parsers/checksum.rs
+++ soundcore-lib/src/parsers/checksum.rs
@@ -1,9 +1,5 @@
-
-
-use nom::{
-    combinator::{map_opt},
-    error::context,
-};
+use log::warn;
+use nom::{combinator::map_opt, error::context};
 
 use super::{take_bytes_from_end, SoundcoreParseError, SoundcoreParseResult};
 
@@ -12,12 +8,14 @@ pub fn parse_and_check_checksum<'a, E: SoundcoreParseError<'a>>(
 ) -> SoundcoreParseResult<(), E> {
     context(
         "parse_and_check_checksum",
-        map_opt(take_bytes_from_end(2usize), |last_bytes| {
+        map_opt(take_bytes_from_end(1usize), |last_bytes| {
             let checksum = generate_checksum(&bytes[..bytes.len() - 1]);
-            match last_bytes == [0x00, checksum] {
-                // TODO: Padding bit??
+            match last_bytes[0] == checksum {
                 true => Some(()),
-                false => None,
+                false => {
+                    warn!("Checksum mismatch: {:?} != {:?}", last_bytes[0], checksum);
+                    None
+                }
             }
         }),
     )(bytes)
@@ -27,7 +25,9 @@ fn generate_checksum<'a, I>(bytes: I) -> u8
 where
     I: IntoIterator<Item = &'a u8>,
 {
-    bytes.into_iter().fold(0, |acc, x| acc.wrapping_add(*x))
+    bytes
+        .into_iter()
+        .fold(0, |acc: u8, x: &u8| acc.wrapping_add(*x))
 }
 
 #[cfg(test)]
@@ -37,7 +37,7 @@ mod checksum_parser {
     #[test]
     fn parses_and_checks_checksum() {
         let input = [0x01, 0x02, 0x00, 0x00, 0x00, 0x03];
-        let expected: (&[u8], ()) = (&[0x01, 0x02, 0x00, 0x00], ());
+        let expected: (&[u8], ()) = (&[0x01, 0x02, 0x00, 0x00, 0x00], ());
         let actual = parse_and_check_checksum::<nom::error::VerboseError<&[u8]>>(&input);
         assert!(actual.is_ok());
         assert_eq!(expected, actual.unwrap());
