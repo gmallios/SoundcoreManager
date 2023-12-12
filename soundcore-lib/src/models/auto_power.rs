bf72f9b363f8b1a2ddfba7d018e9fdd17eedcb94
Gregory Mallios <gregmallios@gmail.com>
2023-12-12 20:13:42 +0200

    fix: Rename auto_power and fix parser

--- soundcore-lib/src/models/auto_power.rs
+++ soundcore-lib/src/models/auto_power.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Ord, PartialOrd, Clone, Copy, Hash)]
 #[serde(rename_all = "camelCase", tag = "type")]
-pub struct AutoPowerOffOn {
+pub struct AutoPowerOff {
     pub enabled: bool,
     pub index: u8, //TODO: Search for possible values and map to enum
 }
--- soundcore-lib/src/parsers/auto_power.rs
+++ soundcore-lib/src/parsers/auto_power.rs
@@ -1,16 +1,16 @@
 use nom::{combinator::map, error::context, number::complete::le_u8, sequence::tuple};
 
-use crate::models::AutoPowerOffOn;
+use crate::models::AutoPowerOff;
 
 use super::{parse_bool, ParseError, ParseResult};
 
 pub fn parse_auto_power_off_on<'a, E: ParseError<'a>>(
     bytes: &'a [u8],
-) -> ParseResult<AutoPowerOffOn, E> {
+) -> ParseResult<AutoPowerOff, E> {
     context(
         "parse_auto_power_off_on",
         map(tuple((parse_bool, le_u8)), |(enabled, index)| {
-            AutoPowerOffOn { enabled, index }
+            AutoPowerOff { enabled, index: index.clamp(0, 3) }
         }),
     )(bytes)
 }
