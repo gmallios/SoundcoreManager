aa94c11a945fadcddcd8b2dbfaf2414a8af07ac4
Gregory Mallios <gregmallios@gmail.com>
2024-02-18 21:31:57 +0200

    fix: Fix EQ bands in HearID

--- soundcore-lib/src/parsers/hearid.rs
+++ soundcore-lib/src/parsers/hearid.rs
@@ -10,49 +10,55 @@ use crate::models::{BaseHearID, CustomHearID, HearIDMusicType, HearIDType, MonoE
 
 use super::{parse_bool, parse_mono_eq, parse_stereo_eq, ParseError, ParseResult};
 
-pub fn parse_base_hear_id<'a, E: ParseError<'a>>(bytes: &'a [u8]) -> ParseResult<BaseHearID, E> {
-    context(
-        "parse_base_hear_id",
-        map(
-            tuple((parse_bool, parse_stereo_eq, le_i32)),
-            |(enabled, eq, time)| BaseHearID {
-                enabled,
-                values: eq,
-                time,
-            },
-        ),
-    )(bytes)
+pub fn parse_base_hear_id<'a, E: ParseError<'a>>(
+    eq_bands: usize,
+) -> impl Fn(&'a [u8]) -> ParseResult<BaseHearID, E> {
+    move |bytes| {
+        context(
+            "parse_base_hear_id",
+            map(
+                tuple((parse_bool, parse_stereo_eq(eq_bands), le_i32)),
+                |(enabled, eq, time)| BaseHearID {
+                    enabled,
+                    values: eq,
+                    time,
+                },
+            ),
+        )(bytes)
+    }
 }
 pub fn parse_custom_hear_id<'a, E: ParseError<'a>>(
-    bytes: &'a [u8],
-) -> ParseResult<CustomHearID, E> {
-    context(
-        "parse_custom_hear_id",
-        map(
-            tuple((
-                parse_base_hear_id,
-                parse_type,
-                parse_music_type,
-                take(8usize),
-                parse_mono_eq,
-            )),
-            |(base, hearid_type, hearid_music_type, l_eq, r_eq)| {
-                let eq = match l_eq[0] {
-                    255 => None,
-                    _ => Some(StereoEQ {
-                        left: MonoEQ::from_bytes(l_eq.try_into().unwrap()),
-                        right: r_eq,
-                    }),
-                };
-                CustomHearID {
-                    base,
-                    hearid_type,
-                    hearid_music_type,
-                    custom_values: eq,
-                }
-            },
-        ),
-    )(bytes)
+    eq_bands: usize,
+) -> impl Fn(&'a [u8]) -> ParseResult<CustomHearID, E> {
+    move |bytes| {
+        context(
+            "parse_custom_hear_id",
+            map(
+                tuple((
+                    parse_base_hear_id(eq_bands),
+                    parse_type,
+                    parse_music_type,
+                    take(8usize),
+                    parse_mono_eq(eq_bands),
+                )),
+                |(base, hearid_type, hearid_music_type, l_eq, r_eq)| {
+                    let eq = match l_eq[0] {
+                        255 => None,
+                        _ => Some(StereoEQ {
+                            left: MonoEQ::from_bytes(l_eq.try_into().unwrap()),
+                            right: r_eq,
+                        }),
+                    };
+                    return CustomHearID {
+                        base,
+                        hearid_type,
+                        hearid_music_type,
+                        custom_values: eq,
+                    };
+                },
+            ),
+        )(bytes)
+    }
 }
 
 fn parse_type<'a, E: ParseError<'a>>(bytes: &'a [u8]) -> ParseResult<HearIDType, E> {
