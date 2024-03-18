0a11b4a0c7a6874545b3cf6cc7cfb6553132e62c
Gregory Mallios <gregmallios@gmail.com>
2024-03-18 20:18:22 +0200

    feat: add vite-plugin-eslint

--- manager-ui/vite.config.ts
+++ manager-ui/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), eslint()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
