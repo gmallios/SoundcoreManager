20dc538e33b86d7bae6ae7b9363288c294a3d23f
Gregory Mallios <gregmallios@gmail.com>
2024-03-18 23:19:09 +0200

    fix: cleanup SoundcoreLibError

--- soundcore-lib/src/error.rs
+++ soundcore-lib/src/error.rs
@@ -69,7 +69,8 @@ pub enum SoundcoreLibError {
     IncompatibleResponse,
     #[error("Invalid MAC address: {addr}")]
     InvalidMACAddress { addr: String },
-    #[cfg(all(target_os = "windows"))]
+    // TODO: Remove btleplug-backend feature when device name resolution is fixed *see scanner.rs*
+    #[cfg(all(target_os = "windows", any(feature = "btleplug-backend", feature = "winrt-backend")))]
     #[error("Unknown Windows Error")]
     UnknownWindowsError {
         #[from]
@@ -95,37 +96,3 @@ impl From<nom::Err<nom::error::VerboseError<&[u8]>>> for SoundcoreLibError {
         }
     }
 }
-
-// impl std::fmt::Display for SoundcoreError {
-//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         match self {
-//             SoundcoreError::Unknown => write!(f, "Unknown error"),
-//             SoundcoreError::ParseError => write!(f, "Parse error"),
-//             SoundcoreError::ResponseChecksumError => write!(f, "Response checksum error"),
-//             SoundcoreError::SendError => write!(f, "Send error"),
-//             SoundcoreError::RecvError => write!(f, "Recv error"),
-//             SoundcoreError::FeatureNotSupported(feat) => write!(f, "Feature not supported/implemented: {}", feat),
-//             SoundcoreError::WinError(e) => write!(f, "Win32 error: {}", e),
-//         }
-//     }
-// }
-
-// impl std::error::Error for SoundcoreError {}
-
-// impl From<std::io::Error> for SoundcoreError {
-//     fn from(_error: std::io::Error) -> Self {
-//         SoundcoreError::Unknown
-//     }
-// }
-
-// impl From<std::num::ParseIntError> for SoundcoreError {
-//     fn from(_error: std::num::ParseIntError) -> Self {
-//         SoundcoreError::Unknown
-//     }
-// }
-
-// impl From<std::string::FromUtf8Error> for SoundcoreError {
-//     fn from(_error: std::string::FromUtf8Error) -> Self {
-//         SoundcoreError::ParseError
-//     }
-// }
