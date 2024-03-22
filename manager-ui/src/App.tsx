82d83296b086822a0f285b65f83e6e4b97a57761
Gregory Mallios <gregmallios@gmail.com>
2024-03-23 01:35:37 +0200

    fix: disconnect all devices only on hard-refresh

--- manager-ui/src/App.tsx
+++ manager-ui/src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { useSoundcoreStore } from './stores/useSoundcoreStore';
 import { BluetoothSearchScreen } from './screens/bluetoothSearch';
-import { useAsyncBridgeEvent } from './hooks/useAsyncBridge';
+import { useAsyncBridgeEvent, useAsyncBridgeRequest } from './hooks/useAsyncBridge';
 import { useShallow } from 'zustand/react/shallow';
+import { DeviceStateScreen } from '@screens/deviceState';
 
 export const App: React.FC = () => {
-  const [handleAsyncBridgeEvent, connectedDevices] = useSoundcoreStore(
-    useShallow((state) => [state.handleAsyncBridgeEvent, state.connectedDevices])
+  const [isFirstRender, setFirstRender] = React.useState(true);
+  const [handleAsyncBridgeEvent, connectedAddresses] = useSoundcoreStore(
+    useShallow((state) => [state.handleAsyncBridgeEvent, state.connectedAddresses])
   );
+
+  const state = useSoundcoreStore((state) => state);
+  console.log('state', state);
 
   // Add the event listener to the bridge, which listener is
   // provided by the store.
@@ -16,9 +21,17 @@ export const App: React.FC = () => {
     handleAsyncBridgeEvent(event);
   });
 
+  // Disconnect all devices on hard refresh
+  useEffect(() => {
+    if (isFirstRender) {
+      useAsyncBridgeRequest({ command: 'disconnectAll' });
+    }
+    setFirstRender(false);
+  }, []);
+
   return (
     <React.Fragment>
-      {connectedDevices.length !== 0 ? <h1>Some connected devices!</h1> : <BluetoothSearchScreen />}
+      {connectedAddresses.size !== 0 ? <DeviceStateScreen /> : <BluetoothSearchScreen />}
     </React.Fragment>
   );
 };
