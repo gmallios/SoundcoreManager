368822c64e6ba6b565f9825b960c10d13b501ed6
Grigoris Mallios <gregmallios@gmail.com>
2024-07-13 00:27:16 +0300

    feat: add console logger for wasm

--- manager-wasm/src/lib.rs
+++ manager-wasm/src/lib.rs
@@ -1,4 +1,5 @@
 #![cfg(target_arch = "wasm32")]
+use log::Level;
 use wasm_bindgen::prelude::*;
 
 mod connection;
@@ -8,4 +9,5 @@ mod web_ble_device;
 #[wasm_bindgen(start)]
 pub fn wasm_init() {
     console_error_panic_hook::set_once();
+    console_log::init_with_level(Level::Trace).expect("error initializing log");
 }
